437380ea0aefe8e252dc96ea4308465d93d305ff
Justin <justin@yan.family>
2024-08-07 23:36:42 -0700

    Adding casing, devices, and a first cut at profiling

--- src/tencheck/examples.py
+++ src/tencheck/examples.py
@@ -15,6 +15,33 @@ class SimpleLinReluModule(nn.Module):
         return x
 
 
+class CasedLinReluModule(nn.Module):
+    _tencheck_cases = [{"out_features": 10}, {"out_features": 20}]
+
+    def __init__(self, out_features: int) -> None:
+        super(CasedLinReluModule, self).__init__()
+        self.linear = nn.Linear(32, out_features)
+        self.relu = nn.ReLU()
+
+    def forward(self, x: Float[torch.Tensor, "B 32"]) -> Float[torch.Tensor, "B O"]:
+        x = self.linear(x)
+        x = self.relu(x)
+        return x
+
+
+class UnusedParamsModule(nn.Module):
+    def __init__(self, out_features: int) -> None:
+        super(UnusedParamsModule, self).__init__()
+        self.linear = nn.Linear(32, out_features)
+        self.unused_linear = nn.Linear(32, out_features)
+        self.relu = nn.ReLU()
+
+    def forward(self, x: Float[torch.Tensor, "B 32"]) -> Float[torch.Tensor, "B O"]:
+        x = self.linear(x)
+        x = self.relu(x)
+        return x
+
+
 class BrokenModule(nn.Module):
     def __init__(self) -> None:
         super(BrokenModule, self).__init__()
@@ -25,4 +52,16 @@ class BrokenModule(nn.Module):
         x = self.linear(x)
         raise Exception("Module is broken")
         x = self.relu(x)  # type: ignore[unreachable]
+        return x
+
+
+class MistypedModule(nn.Module):
+    def __init__(self, out_features: int) -> None:
+        super(MistypedModule, self).__init__()
+        self.linear = nn.Linear(32, out_features)
+        self.relu = nn.ReLU()
+
+    def forward(self, x: Float[torch.Tensor, "B 32"]) -> Float[torch.Tensor, "B 1025"]:
+        x = self.linear(x)
+        x = self.relu(x)
         return x
